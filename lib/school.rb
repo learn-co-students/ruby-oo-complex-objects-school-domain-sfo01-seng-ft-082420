0bbc6198d5f2129ea90922272162ddd71568b9ea
mehmehmehlol <hkmeganlo@gmail.com>
2020-08-25 23:59:47 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+require 'pry'
+
+class School
+    attr_accessor :roster, :name
+
+    def initialize(roster)
+        @roster = {}
+        @name = name
+    end
+
+    def add_student(student_name, grade)
+        @roster[grade] ||= []
+        @roster[grade] << student_name
+    end
+
+    def grade(student_grade)
+        @roster[student_grade]
+    end
+
+    def sort
+        sorted = {}
+        @roster.each do |grade, name_array|
+            sorted[grade] = name_array.sort
+        end
+        sorted
+    end 
+end
+
+
+# school = School.new("Bayside High School")
