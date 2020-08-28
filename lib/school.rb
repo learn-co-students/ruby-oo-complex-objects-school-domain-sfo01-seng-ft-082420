cea37a79deab3bb7018bba8caa6b65522242d64d
Will-Gon <will.colocho@gmail.com>
2020-08-27 20:35:29 -0700

    passed

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require 'pry'
+class School
+    attr_reader
+    attr_writer 
+    attr_accessor :school, :roster
+
+    def initialize(school)
+        @school = school
+        @roster = {}
+        #binding.pry
+    end
+
+    def add_student(student, grade)
+        #binding.pry
+            (@roster[grade] ||= []) << student
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        @roster.each do |grade, students|
+            students.sort!
+        end
+    end
+end
+
+#binding.pry
+0
